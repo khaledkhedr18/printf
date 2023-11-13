3e8b4488448d615b59c1ea063f275f2d694136d9
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-11-13 22:10:04 +0200

    task 0

--- 0-format.c
+++ 0-format.c
@@ -16,12 +16,12 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	while(*format != '\0')
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
 			format++;
-			switch(*format)
+			switch (*format)
 			{
 				case 'c':
 				count += printf("%c", va_arg(args, int));
@@ -43,6 +43,6 @@ int _printf(const char *format, ...)
 		format++;
 	}
 	va_end(args);
-	return count;
+	return (count);
 
 }
